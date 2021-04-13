ad925bb49556a6878b6b742401b5a6ecd836d1cf
Yin Song <songniy@gmail.com>
2021-04-12 21:18:06 -0400

    bugfix: added script to serve bundle back to index.html

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
-// establishes socket connection
+
 ReactDOM.render(
       <App />,
   document.getElementById('app')
